8506b0973fe892227584b0a4ef7d5ea17bb022bd
Leon Zagorouiko & Kim Dang <student@epicodus.com>
2015-05-13 14:33:52 -0700

    Add checkbox to join recipe and categories.

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ get('/') do
   erb(:index)
 end
 
+#---------------post new category
 post('/') do
   description = params.fetch('description')
   Category.create({:description => description})
@@ -15,6 +16,7 @@ post('/') do
   erb(:index)
 end
 
+#----------------category page with recipes
 get('/category/:id/recipes') do
   id = params.fetch('id').to_i
   @category = Category.find(id)
@@ -22,6 +24,7 @@ get('/category/:id/recipes') do
   erb(:recipes)
 end
 
+#---------------------delete a category
 delete('/category/:id') do
   id = params.fetch('id').to_i
   category = Category.find(id)
@@ -30,6 +33,7 @@ delete('/category/:id') do
   erb(:index)
 end
 
+#--------------------one recipe page
 get('/category/:category_id/recipe/:id') do
   @id = params.fetch('id').to_i
   category_id = params.fetch('category_id').to_i
@@ -38,6 +42,7 @@ get('/category/:category_id/recipe/:id') do
   erb(:recipes)
 end
 
+#---------------------create a new recipe
 get('/recipes/new') do
   @categories = Category.all
   erb(:recipe_form)
@@ -46,9 +51,21 @@ end
 post('/recipes') do
   name = params.fetch('name')
   instruction = params.fetch('instruction')
-  category_id = params.fetch('category_id').to_i
-  @category = Category.find(category_id)
+  category_ids = params.fetch('category_ids')
   new_recipe = Recipe.create({:name => name, :instruction => instruction})
+  length = category_ids.length()
+  length.times() do |time|
+    id = category_ids[time].to_i
+    category = Category.find(id)
+    category.recipes.push(new_recipe)
+  end
+  @categories = Category.all
+  erb(:index)
+end
+
+#--------------------update a recipe
+patch('/recipes/:id/edit') do
+  @category = Category.find(category_id)
   @category.recipes.push(new_recipe)
   @recipes = @category.recipes()
   erb(:recipes)
