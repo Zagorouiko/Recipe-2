beb44315cc45751e9a165767ab66ae6fd2b57671
Leon Zagorouiko & Kim Dang <student@epicodus.com>
2015-05-13 11:37:23 -0700

    Adds recipes.

---
+++ app.rb
@@ -0,0 +1,45 @@
+require("bundler/setup")
+Bundler.require(:default)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @categories = Category.all
+  erb(:index)
+end
+
+post('/') do
+  description = params.fetch('description')
+  Category.create({:description => description})
+  erb(:index)
+end
+
+get('/category/:id/recipes') do
+  id = params.fetch('id').to_i
+  @category = Category.find(id)
+  @recipes = @category.recipes()
+  erb(:recipes)
+end
+
+get('/category/:category_id/recipe/:id') do
+  @id = params.fetch('id').to_i
+  category_id = params.fetch('category_id').to_i
+  category = Category.find(category_id)
+  @recipes = category.recipes()
+  erb(:recipes)
+end
+
+get('/recipes/new') do
+  @categories = Category.all
+  erb(:recipe_form)
+end
+
+post('/recipes') do
+  name = params.fetch('name')
+  instruction = params.fetch('instruction')
+  category_id = params.fetch('category_id').to_i
+  category = Category.find(category_id)
+  new_recipe = Recipe.create({:name => name, :instruction => instruction})
+  category.recipes.push(new_recipe)
+  @recipes = category.recipes()
+  erb(:recipes)
+end
