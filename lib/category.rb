f35228bfacb6c7e16604651abb8b9af7655c37e3
Kim Dang & Leon Zagorouiko <student@epicodus.com>
2015-05-14 12:28:22 -0700

    Fix background image.

--- lib/category.rb
+++ lib/category.rb
@@ -1,4 +1,12 @@
 class Category < ActiveRecord::Base
-  has_and_belongs_to_many(:recipes)
   validates(:description, {:presence => true, :length => {:maximum => 50}})
+  before_save(:capitalize_description)
+  has_and_belongs_to_many(:recipes)
+
+  private
+
+  define_method(:capitalize_description) do
+    self.description=(description().capitalize())
+  end
+
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,5 +1,13 @@
 class Recipe < ActiveRecord::Base
-  has_and_belongs_to_many(:categories)
   validates(:name, {:presence => true, :length => {:maximum => 50}})
   validates(:instruction, {:presence => true})
+  before_save(:capitalize_name)
+  has_and_belongs_to_many(:categories)
+
+  private
+
+  define_method(:capitalize_name) do
+    self.name=(name().capitalize())
+  end
+
 end
